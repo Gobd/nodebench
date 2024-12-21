8549b0431137bc5b6121dd89431671addb5eb04e
Brian Kemper <bkemper@gmail.com>
2024-12-21 11:15:01 -0700

    .

--- README.md
+++ README.md
@@ -6,11 +6,14 @@
 ./start.sh
 ```
 
- - TODO something to make graphs -- output html with a js library, maybe imges too
- - TODO expose all ports
+ - TODO something to make graphs
+   - https://www.chartjs.org/
+   - output html with a js library, maybe imges too
+   - https://quickchart.io/documentation/chart-js/image-export/
+ - TODO expose all ports from docker-compose
  - TODO fix arm64 hardcode in encore/build.sh
 
-one with everything
+one graph with all runtimes & routers
 req/s
 avg resp time
 max resp time
--- bench.sh
+++ bench.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3000 > /results/hono-bun.txt
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3001 > /results/express-bun.txt
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3003 > /results/elysia-bun.txt
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3000 > /results/hono-bun.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3001 > /results/express-bun.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3003 > /results/elysia-bun.json
 
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3004 > /results/hono-node.txt
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3005 > /results/express-node.txt
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3006 > /results/fastify-node.txt
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3004 > /results/hono-node.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3005 > /results/express-node.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3006 > /results/fastify-node.json
 
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://deno:3008 > /results/hono-deno.txt
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://deno:3009 > /results/express-deno.txt
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://deno:3008 > /results/hono-deno.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://deno:3009 > /results/express-deno.json
 
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://encore:8080 > /results/encore-json.txt
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://encore:8080/raw > /results/encore-raw.txt
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://encore:8080 > /results/encore-json.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://encore:8080/raw > /results/encore-raw.json
