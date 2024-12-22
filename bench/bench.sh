01c20e2d4e917ce3e5c1f6441a2f6d5551a24f5f
Brian Kemper <bkemper@gmail.com>
2024-12-22 14:12:57 -0700

    Add hattip and H3 to bun

--- bench/bench.sh
+++ bench/bench.sh
@@ -3,6 +3,8 @@
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3000 > /results/hono-bun.json
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3001 > /results/express-bun.json
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3003 > /results/elysia-bun.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3020 > /results/h3-bun.json
+bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://bun:3021 > /results/hattip-bun.json
 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3004 > /results/hono-node.json
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://node:3005 > /results/express-node.json
@@ -12,4 +14,4 @@ bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://deno:3008 > /results/hon
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://deno:3009 > /results/express-deno.json
 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://encore:8080 > /results/encore-json.json
-bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://encore:8080/raw > /results/encore-raw.json
+# bombardier -c 200 -l -d 30s --fasthttp -p r -o j http://encore:8080/raw > /results/encore-raw.json
--- runtimes/bun/bun.js
+++ runtimes/bun/bun.js
@@ -24,3 +24,32 @@ import { Elysia } from "elysia";
 const elysiaApp = new Elysia();
 elysiaApp.get("/", text);
 elysiaApp.listen(3003);
+
+// H3 Bun 3020
+import {
+  createApp,
+  createRouter as h3CreateRouter,
+  defineEventHandler,
+  toWebHandler,
+} from "h3";
+const h3Router = h3CreateRouter();
+const h3App = createApp();
+h3App.use(h3Router);
+h3Router.get(
+  "/",
+  defineEventHandler(() => {
+    return text;
+  }),
+);
+Bun.serve({
+  port: 3020,
+  fetch: toWebHandler(h3App),
+});
+
+// Hattip Bun 3021
+import bunAdapter from "@hattip/adapter-bun";
+import { text as hattipText } from "@hattip/response";
+import { createRouter as hattipCreateRouter } from "@hattip/router";
+const hattipApp = hattipCreateRouter();
+hattipApp.get("/", () => hattipText(text));
+Bun.serve(bunAdapter(hattipApp.buildHandler(), { port: 3021 }));
