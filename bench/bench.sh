e1ed3b4ac4a1dfe7b93c530ef1df10bab8d65258
Brian Kemper <bkemper@gmail.com>
2025-02-16 13:28:08 -0700

    .

--- bench/bench.sh
+++ bench/bench.sh
@@ -2,35 +2,35 @@
 
 curl "http://bun:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://bun:${HONO_PORT}" > /results/hono-bun.json
-curl "http://bun:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://bun:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://bun:${EXPRESS_PORT}" > /results/express-bun.json
-curl "http://bun:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://bun:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://bun:${ELYSIA_PORT}" > /results/elysia-bun.json
-curl "http://bun:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://bun:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://bun:${H3_PORT}" > /results/h3-bun.json
-curl "http://bun:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://bun:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://bun:${HATTIP_PORT}" > /results/hattip-bun.json
-curl "http://bun:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://bun:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://bun:${FASTIFY_PORT}" > /results/fastify-bun.json
-curl "http://bun:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://bun:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://bun:${BUN_PORT}" > /results/bun-bun.json
 
-curl "http://node:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://node:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://node:${HONO_PORT}" > /results/hono-node.json
-curl "http://node:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://node:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://node:${EXPRESS_PORT}" > /results/express-node.json
-curl "http://node:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://node:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://node:${ELYSIA_PORT}" > /results/elysia-node.json
-curl "http://node:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://node:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://node:${FASTIFY_PORT}" > /results/fastify-node.json
 
-curl "http://deno:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://deno:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://deno:${HONO_PORT}" > /results/hono-deno.json
-curl "http://deno:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://deno:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://deno:${ELYSIA_PORT}" > /results/elysia-deno.json
-curl "http://deno:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://deno:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://deno:${EXPRESS_PORT}" > /results/express-deno.json
-curl "http://deno:${HONO_PORT}/gc" 
+curl --fail-with-body --silent --show-error "http://deno:${HONO_PORT}/gc" 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://deno:${FASTIFY_PORT}" > /results/fastify-deno.json
 
 bombardier -c 200 -l -d 30s --fasthttp -p r -o j "http://encore:${ENCORE_PORT}" > /results/encore-json.json
